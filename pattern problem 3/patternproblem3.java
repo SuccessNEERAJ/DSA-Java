270d0821ebabd9c48e7edb6c196a34667f0b45a3
SuccessNEERAJ <113997753+SuccessNEERAJ@users.noreply.github.com>
2025-08-13 11:50:55 +0530

    pattern problem  3

---
+++ pattern problem 3/patternproblem3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class patternproblem3{
+    public static void main(String[] args) {
+        System.out.println("Enter a odd number for the pattern which is greater than 3");
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        int trows = n;
+        int stars = 1;
+        for(int row =1; row<=trows; row++){
+            //work for the current row
+            for(int cst = 1; cst<=stars; cst++){
+                System.out.print("*");
+            }
+            //prepare for the next row
+            System.out.println();
+            if(row <= trows / 2){
+                stars++;
+            }else{
+                stars--;
+            }
+        }
+
+    }
+}
---
+++ pattern problem 3/readme.md
@@ -0,0 +1,15 @@
+# Diamond Half Pattern in Java
+
+## 📌 Problem Statement
+Write a Java program that takes an **odd number greater than 3** as input and prints a star pattern where:
+- The number of stars increases until the middle row.
+- Then decreases symmetrically.
+
+For example, if `n = 5`, the output should be:
+<pre>
+*
+**
+***
+**
+*
+</pre>
